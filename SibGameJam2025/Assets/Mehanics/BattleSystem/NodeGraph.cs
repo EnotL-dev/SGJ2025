c8c5c5dcddeb93b674221d636afcbe2a11923295
EnotL-dev <enotyzer@gmail.com>
2025-11-01 22:35:19 +0300

    input fixed

---
+++ SibGameJam2025/Assets/Mehanics/BattleSystem/NodeGraph.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleSystem
+{
+    public class NodeGraph
+    {
+        public Shape shape;
+        public Impact impact;
+        public Feature feature;
+    }
+}
---
+++ SibGameJam2025/Assets/Mehanics/BattleSystem/Shape.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace BattleSystem
+{
+    [CreateAssetMenu(fileName = "Node", menuName = "Nodes/Shape")]
+    public abstract class Shape : Node
+    {
+        
+    }
+}
