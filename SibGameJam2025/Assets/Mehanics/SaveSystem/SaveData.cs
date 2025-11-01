6d5e142764130922c375aac1738e07eb94b92564
EnotL-dev <enotyzer@gmail.com>
2025-11-01 21:04:48 +0300

    SceneLoader update
    SaveSystem
    SoundSettings

---
+++ SibGameJam2025/Assets/Mehanics/SaveSystem/SaveData.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaveSystem
+{
+    public static class SaveData
+    {
+        public static Dictionary<string, float> Volumes = new Dictionary<string, float>();
+        //public static int indexScene = 0; //индекс сцены игрока, при переходе уровня +1
+    }
+}
