9f2a39395d87abe151fa0b3abbd9fd9d1741f2a0
EnotL-dev <enotyzer@gmail.com>
2025-11-02 23:25:04 +0300

    Playerpref update

---
+++ SibGameJam2025/Assets/Mehanics/BattleSystem/SpellCollision.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SpellCollision : MonoBehaviour
+{
+    public int damage;
+    [SerializeField] private LayerMask _layerMaskHit;
+    [SerializeField] private LayerMask _layerMaskIgnore;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsLayerInMask(other.gameObject.layer, _layerMaskIgnore))
+            return;
+        if (IsLayerInMask(other.gameObject.layer, _layerMaskHit))
+        {
+            if (other.gameObject.TryGetComponent(out Health health))
+            {
+                health.Reduce(damage);
+            }
+        }
+    }
+
+    public static bool IsLayerInMask(int layer, LayerMask layerMask)
+    {
+        return layerMask == (layerMask | (1 << layer));
+    }
+}
