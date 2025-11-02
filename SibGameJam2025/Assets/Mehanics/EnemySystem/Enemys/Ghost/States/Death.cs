c5196f5747eb4f26c49555699462c3e7c7bb7f74
Liho <84248601+LihoDev@users.noreply.github.com>
2025-11-02 21:42:09 +0300

    Ghost fixes

--- SibGameJam2025/Assets/Mehanics/EnemySystem/Enemys/Ghost/States/Death.cs
+++ SibGameJam2025/Assets/Mehanics/EnemySystem/Enemys/Ghost/States/Death.cs
@@ -1,17 +1,17 @@
-using UnityEngine;
-
-
 namespace EnemySystem.Ghost
 {
     public class Death : State
     {
-        public Death(IStateSwitcher stateSwitcher) : base(stateSwitcher)
+        private AnimatorController _animator;
+
+        public Death(IStateSwitcher stateSwitcher, AnimatorController animator) : base(stateSwitcher)
         {
+            _animator = animator;
         }
 
         public override void Start()
         {
-           
+            _animator.CallDeath();
         }
 
         public override void Stop()
--- SibGameJam2025/Assets/Mehanics/EnemySystem/Enemys/Ghost/States/GhostStates.cs
+++ SibGameJam2025/Assets/Mehanics/EnemySystem/Enemys/Ghost/States/GhostStates.cs
@@ -10,18 +10,37 @@ namespace EnemySystem.Ghost
         [SerializeField] private float _distanceDetect = 10f;
         [SerializeField] private Collider _collision;
         [SerializeField] private RotateToPlayer _rotor;
+        [SerializeField] private Health _health;
 
         protected override void InitializeStates()
         {
             _states.Add(new Waiting(this, transform, PlayerRefs.Instance.CharacterController, _animator, _distanceDetect, _rotor));
             _states.Add(new Moving(this, transform, PlayerRefs.Instance.CharacterController, _config, _animator, _distanceDetect, _rotor));
             _states.Add(new Attack(this, transform, PlayerRefs.Instance.CharacterController, _config, _animator, PlayerRefs.Instance.Health, _rotor));
+            _states.Add(new Death(this, _animator));
             SwitchState<Waiting>();
         }
 
         protected override void OnAwake()
         {
             
+        }
+
+        private void OnEnable()
+        {
+            _health.IsOver += CallDeath;
+        }
+
+        private void OnDisable()
+        {
+            _health.IsOver -= CallDeath;
+        }
+
+        private void CallDeath()
+        {
+            SwitchState<Death>();
+            _collision.enabled = false;
+            _rotor.enabled = false;
         }
     }
 }
