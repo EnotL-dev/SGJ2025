3c3b9b7a263eda25347cf7e07fa31ea24efce0a8
EnotL-dev <enotyzer@gmail.com>
2025-11-03 17:42:04 +0300

    WellScript update

--- SibGameJam2025/Assets/Mehanics/LevelSystem/WellScript.cs
+++ SibGameJam2025/Assets/Mehanics/LevelSystem/WellScript.cs
@@ -10,26 +10,5 @@ namespace LevelSystem
         public GameObject canvas;
         public TextMeshProUGUI textFountainSouls;
         public Slider sliderFountainSouls;
-
-        Transform playerTransform;
-        private void Start()
-        {
-            playerTransform = FindFirstObjectByType<PlayerRefs>().transform;
-        }
-
-        private void Update()
-        {
-            if (playerTransform)
-            {
-                Vector3 direction = playerTransform.position - canvas.transform.position;
-                direction.y = 0;
-
-                if (direction != Vector3.zero)
-                {
-                    canvas.transform.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180, 0);
-                }
-                print("1");
-            }
-        }
     }
 }
