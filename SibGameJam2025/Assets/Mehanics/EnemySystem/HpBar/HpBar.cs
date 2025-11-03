850698b51db701ec39c3e69bdddbddcc6e47554d
Liho <84248601+LihoDev@users.noreply.github.com>
2025-11-03 03:18:09 +0300

    Added HpBar

--- SibGameJam2025/Assets/Mehanics/EnemySystem/HpBar/HpBar.cs
+++ SibGameJam2025/Assets/Mehanics/EnemySystem/HpBar/HpBar.cs
@@ -1,9 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace EnemySystem
 {
     public class HpBar : MonoBehaviour
     {
-     //   [SerializeField] private 
+        [SerializeField] private Image _image;
+        [SerializeField] private Health _health;
+
+        private void OnEnable()
+        {
+            _health._current.Changed += UpdateValue;
+        }
+
+        private void OnDisable()
+        {
+            _health._current.Changed -= UpdateValue;
+        }
+
+        private void UpdateValue(int old, int value)
+        {
+           _image.fillAmount = (float)value / (float)_health._max.Value;
+        }
     }
 }
